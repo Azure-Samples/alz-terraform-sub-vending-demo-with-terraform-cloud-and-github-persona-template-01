f58afa0d29f33264c1a60aff79fba2f82a7bcb3f
Jared Holgate <jaredholgate@microsoft.com>
2023-10-01 15:20:11 +0100

    Update to use provided RG

--- main.tf
+++ main.tf
@@ -7,29 +7,28 @@ provider "azurerm" {
 }
 
 
-resource "azurerm_resource_group" "example" {
-  name       = "example-resources"
-  location   = "West Europe"
+data "azurerm_resource_group" "example" {
+  name       = var.resource_group_name
 }
 
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "example" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
+  resource_group_name  = data.azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_interface" "example" {
   name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
 
   ip_configuration {
     name                          = "internal"
@@ -40,8 +39,8 @@ resource "azurerm_network_interface" "example" {
 
 resource "azurerm_windows_virtual_machine" "example" {
   name                = "example-machine"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
   size                = "Standard_B2s"
   admin_username      = "adminuser"
   admin_password      = "P@$$w0rd1234!"
